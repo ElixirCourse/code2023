fd93eb05edcd32bb8a3d2da0aad6d2ee786248a3
meddle <n.tzvetinov@gmail.com>
2023-04-17 18:14:46 +0300

    Add distributed app code

--- ecio/lib/ecio/application.ex
+++ ecio/lib/ecio/application.ex
@@ -3,14 +3,19 @@ defmodule ECIO.Application do
 
   use Application
 
+  require Logger
+
   @impl true
-  def start(_type, _args) do
+  def start(type, args) do
+    Logger.info("ECIO Application started with type #{inspect(type)} and args #{inspect(args)}")
+    port = System.get_env("PORT", "5555") |> String.to_integer()
+
     children = [
       {Registry, keys: :duplicate, name: ECIO.PubSub, partitions: System.schedulers_online()},
       ECIO.Context,
       {ECIO.Server, [%{ansi_format: [:red]}]},
       {ECIO.Device, [[]]},
-      {ECIO.SocketListener, [{:port, 5556}]}
+      {ECIO.SocketListener, [{:port, port}]}
     ]
 
     opts = [strategy: :one_for_one, name: ECIO.Supervisor]
